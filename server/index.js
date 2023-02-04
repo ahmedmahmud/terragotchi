5e43b8949ad042261d383e2954445c756db5af9f
maxstu <47148369+maxstu@users.noreply.github.com>
2023-02-04 17:35:58 +0000

    Add support for processing auth, re-auth and activity data

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+let db = {}
+
 // parse application/json
 app.use(bodyParser.json())
 
@@ -13,10 +15,15 @@ app.get('/', (req, res) => {
 
 app.post('/terra_hook', (req, res) => {
   switch (req.body.type) {
-    case "user":
-      
+    case "auth":
+      register_user(req.body.user.user_id);
       break;
-  
+    case "user_reauth":
+      update_user();
+      break;
+    case "activity":
+      load_activity_data(req.body.user.user_id, req.body.MET_data);
+      break;
     default:
       break;
   }
@@ -26,3 +33,25 @@ app.post('/terra_hook', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+register_user = (uid) => {
+  db[uid] = {
+    "health": 0,
+    "environment": 0
+  }
+  console.log(db)
+}
+
+update_user = (old_uid, new_uid) => {
+  db[new_uid] = db[old_uid];
+  delete db[old_uid];
+}
+
+load_activity_data = (uid, data) => {
+  let moderate_minutes = data.num_moderate_intensity_minutes;
+  let high_minutes = data.num_high_intensity_minutes;
+
+  let score = Math.floor(moderate_minutes / 10 + high_minutes / 3)
+
+  db[uid].health += score;
+}
