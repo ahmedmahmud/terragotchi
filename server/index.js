e531045d798b5c4c84b8e6bb58439fc46b4ff749
Ahmed Mahmud <am5321@ic.ac.uk>
2023-02-04 16:34:10 +0000

    add terra webhook handler, print back json

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.json('Hello World!')
+  res.send('Terragotchi server')
+})
+
+app.get('/terra_hook', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
 })
 
 app.listen(port, () => {
